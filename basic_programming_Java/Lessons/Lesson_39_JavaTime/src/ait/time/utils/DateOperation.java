307bbd5c487f66899b0b5b7b603a543bfb529a69
ihor737 <ihor737@gmail.com>
2023-10-31 13:13:41 +0100

    Lesson 39 added.

---
+++ basic_programming_Java/Lessons/Lesson_39_JavaTime/src/ait/time/utils/DateOperation.java
@@ -0,0 +1,17 @@
+package ait.time.utils;/*
+ * Copyright (c) 2023.
+ * Igor Volotovskyi
+ */
+
+public class DateOperation {
+    public static int getAge(String birthDate){
+        //TODO
+
+        return 0;
+    }
+
+    public static String [] sortStringDates(String [] dates){
+        //TODO
+        return null;
+    }
+}
