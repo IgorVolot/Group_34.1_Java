d7c3342142644021ca76e72ce262fdf769efd611
ihor737 <ihor737@gmail.com>
2023-11-06 12:15:55 +0100

    Lesson 41 added

---
+++ basic_programming_Java/Lessons/Lesson_41_AlbumDateSorted/src/ait/album/dao/AlbumImpl.java
@@ -0,0 +1,96 @@
+/*
+ * Copyright (c) 2023.
+ * Author: Igor Volotovskyi
+ */
+
+package ait.album.dao;
+
+import ait.album.model.Photo;
+
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.function.Predicate;
+
+public class AlbumImpl implements Album {
+    private Photo[] photos;
+    private int size;
+    private Comparator<Photo> comparator = (p1, p2) -> p1.getDate().compareTo(p2.getDate());
+
+    public AlbumImpl(int capacity) {
+        this.photos = new Photo[capacity];
+    }
+
+    @Override
+    public boolean addPhoto(Photo photo) {
+        if (photo == null || getPhotoFromAlbum(photo.getPhotoId(), photo.getAlbumId()) != null
+                || size == photos.length) {
+            return false;
+        }
+        int index = Arrays.binarySearch(photos, 0, size, photo, comparator);
+        index = index >= 0 ? index : -index - 1;
+        System.arraycopy(photos, index, photos, index + 1, size - index);
+        photos[index] = photo;
+        size++;
+        return true;
+    }
+
+    @Override
+    public boolean removePhoto(int photoId, int albumId) {
+        for (int i = 0; i < size; i++) {
+            if (photos[i].getPhotoId() == photoId && photos[i].getAlbumId() == albumId) {
+                photos[i] = getPhotoFromAlbum(photoId, albumId);
+                System.arraycopy(photos, i + 1, photos, i, size - i - i);
+                photos[--size] = null;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public boolean updatePhoto(int photoId, int albumId, String url) {
+        Photo photo = getPhotoFromAlbum(photoId, albumId);
+        if (photo == null) {
+            return false;
+        }
+        photo.setUrl(url);
+        return true;
+    }
+
+    @Override
+    public Photo getPhotoFromAlbum(int photoId, int albumId) {
+        for (int i = 0; i < size; i++) {
+            if (photos[i].getPhotoId() == photoId && photos[i].getAlbumId() == albumId) {
+                return photos[i];
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public Photo[] getAllPhotoFromAlbum(int albumId) {
+        return findPhotoByPredicate(p -> p.getAlbumId() == albumId);
+    }
+
+    @Override
+    public Photo[] getPhotoBetweenDate(LocalDate dateFrom, LocalDate dateTo) {
+        return findPhotoByPredicate(p -> p.getDate().toLocalDate().isAfter(dateFrom.minusDays(1)) && p.getDate().toLocalDate().isBefore(dateTo.plusDays(1)));
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    private Photo[] findPhotoByPredicate(Predicate<Photo> predicate) {
+        Photo[] res = new Photo[size];
+        int j = 0;
+        for (int i = 0; i < size; i++) {
+            if (predicate.test(photos[i])) {
+                res[j++] = photos[i];
+            }
+        }
+        return Arrays.copyOf(res, j);
+    }
+}
