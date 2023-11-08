ae9bdab184d358218bf16e5f9ae8e832ec20a206
ihor737 <ihor737@gmail.com>
2023-11-08 09:26:58 +0100

    HM 42 Fibonacci completed.

--- basic_programming_Java/Homework/HW_42_Fibonacci/src/ait/fibonacci/FibonacciAppl.java
+++ basic_programming_Java/Homework/HW_42_Fibonacci/src/ait/fibonacci/FibonacciAppl.java
@@ -2,11 +2,25 @@ package ait.fibonacci;
 
 import ait.fibonacci.model.Fibonacci;
 
+import java.util.Iterator;
+
 public class FibonacciAppl {
     public static void main(String[] args) {
         Fibonacci fibonacci = new Fibonacci(8);
         // TODO print all numbers and calculate sum
         // 1,1,2,3,5,8,13,21
         // sum = 54
+
+
+        Iterator<Integer> iterator = fibonacci.iterator();
+        int sum = 0;
+        while (iterator.hasNext()) {
+            int n = iterator.next();
+            System.out.print(n + " |");
+            sum += n;
+        }
+        System.out.println();
+        System.out.println("Sum = " + sum);
+
     }
 }
--- basic_programming_Java/Homework/HW_42_Fibonacci/src/ait/fibonacci/model/FibonacciIterator.java
+++ basic_programming_Java/Homework/HW_42_Fibonacci/src/ait/fibonacci/model/FibonacciIterator.java
@@ -3,13 +3,47 @@ package ait.fibonacci.model;
 import java.util.Iterator;
 
 public class FibonacciIterator implements Iterator<Integer> {
-    @Override
-    public boolean hasNext() {
-        return false;
+    private int size;
+    private int currPos;
+
+    private int prev;
+    private int prevPrev;
+
+    public FibonacciIterator(int size) {
+        this.size = size;
+        prev = 1;
+        prevPrev = 1;
+        currPos = 1;
     }
 
     @Override
-    public Integer next() {
-        return null;
+    public boolean hasNext() {
+        return currPos <= size;
     }
+
+//    @Override
+//    public Integer next() {
+//        int fibo = 0;
+//        int num1 = currPos;
+//        int num2 = ++currPos;
+//        if (currPos <= 1){
+//            fibo = num1 + num2;
+//        } else {
+//            fibo = (currPos - 2) + (currPos - 1);
+//        }
+//        return fibo;
+//    }
+
+    @Override
+    public Integer next() {
+        if (currPos++ <= 2) {
+            return 1;
+        }
+        int curr = prev;
+        prev = prev + prevPrev;
+        prevPrev = curr;
+        return prev;
+    }
+
+
 }
