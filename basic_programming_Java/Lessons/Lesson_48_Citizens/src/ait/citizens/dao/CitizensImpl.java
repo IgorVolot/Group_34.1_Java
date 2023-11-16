cdcf93fb44092b545ca4d495dfd9b663ca22e2ef
ihor737 <ihor737@gmail.com>
2023-11-16 14:13:30 +0100

    Lesson 48. All tasks are completed.

--- basic_programming_Java/Lessons/Lesson_48_Citizens/src/ait/citizens/dao/CitizensImpl.java
+++ basic_programming_Java/Lessons/Lesson_48_Citizens/src/ait/citizens/dao/CitizensImpl.java
@@ -2,7 +2,9 @@ package ait.citizens.dao;
 
 import ait.citizens.model.Person;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
@@ -10,59 +12,112 @@ public class CitizensImpl implements Citizens {
     private List<Person> idList;
     private List<Person> lastNameList;
     private List<Person> ageList;
-    private static Comparator<Person> lastNameComparator = (p1 , p2) -> p1.getLastName().compareTo(p2.getLastName());
-    private static Comparator<Person> ageComparator = (p1, p2) -> Integer.compare(p1.getAge(), p2.getAge());
+    private static Comparator<Person> lastNameComparator = (p1, p2) -> {
+        int res = p1.getLastName().compareTo(p2.getLastName());
+        return res != 0 ? res : Integer.compare(p1.getId(), p2.getId());
+    };
+    private static Comparator<Person> ageComparator = (p1, p2) -> {
+        int res = Integer.compare(p1.getAge(), p2.getAge());
+        return res != 0 ? res : Integer.compare(p1.getId(), p2.getId());
+    };
 
-    public CitizensImpl(){}
-
-    public CitizensImpl(List<Person> citizens){
+    // O(1)
+    public CitizensImpl() {
         idList = new ArrayList<>();
         lastNameList = new ArrayList<>();
         ageList = new ArrayList<>();
     }
 
+    // O(n)
+    public CitizensImpl(List<Person> citizens) {
+        this();
+        citizens.forEach(p -> add(p));
+    }
+
+    // O(n)
     @Override
     public boolean add(Person person) {
-        return false;
+        if (person == null) {
+            return false;
+        }
+        int index = Collections.binarySearch(idList, person);
+        if (index >= 0) {
+            return false;
+        }
+        index = -index - 1;
+        idList.add(index, person);
+        index = Collections.binarySearch(ageList, person, ageComparator);
+        index = index >= 0 ? index : -index - 1;
+        ageList.add(index, person);
+        index = Collections.binarySearch(lastNameList, person, lastNameComparator);
+        index = index >= 0 ? index : -index - 1;
+        lastNameList.add(index, person);
+        return true;
     }
 
+    // O(n)
     @Override
     public boolean remove(int id) {
-        return false;
+        Person victim = find(id);
+        if (victim == null) {
+            return false;
+        }
+        idList.remove(victim);
+        ageList.remove(victim);
+        lastNameList.remove(victim);
+        return true;
     }
 
+    // O(log(n))
     @Override
     public Person find(int id) {
-        return null;
+        Person pattern = new Person(id, null, null, null);
+        int index = Collections.binarySearch(idList, pattern);
+        return index < 0 ? null : idList.get(index);
     }
 
+    // O(log(n))
     @Override
     public Iterable<Person> find(int minAge, int maxAge) {
-        return null;
+        LocalDate now = LocalDate.now();
+        Person pattern = new Person(Integer.MIN_VALUE, null, null, now.minusYears(minAge));
+        int from = -Collections.binarySearch(ageList, pattern, ageComparator) - 1;
+        pattern = new Person(Integer.MAX_VALUE, null, null, now.minusYears(maxAge));
+        int to = -Collections.binarySearch(ageList, pattern, ageComparator) - 1;
+        return ageList.subList(from, to); // O(1)
     }
 
+    // O(log(n))
     @Override
     public Iterable<Person> find(String lastName) {
-        return null;
+        Person pattern = new Person(Integer.MIN_VALUE, null, lastName, null);
+        int from = -Collections.binarySearch(lastNameList, pattern, lastNameComparator) - 1;
+        pattern = new Person(Integer.MAX_VALUE, null, lastName, null);
+        int to = -Collections.binarySearch(lastNameList, pattern, lastNameComparator) - 1;
+        return lastNameList.subList(from, to);
     }
 
+    // O(log(n))
     @Override
     public Iterable<Person> getAllPersonsSortedById() {
-        return null;
+        return idList;
     }
 
+    // O(log(n))
     @Override
     public Iterable<Person> getAllPersonsSortedByAge() {
-        return null;
+        return ageList;
     }
 
+    // O(log(n))
     @Override
     public Iterable<Person> getAllPersonsSortedByLastName() {
-        return null;
+        return lastNameList;
     }
 
+    // O(1)
     @Override
     public int size() {
-        return 0;
+        return idList.size();
     }
 }
--- basic_programming_Java/Lessons/Lesson_48_Citizens/src/ait/citizens/test/CitizensTest.java
+++ basic_programming_Java/Lessons/Lesson_48_Citizens/src/ait/citizens/test/CitizensTest.java
@@ -6,99 +6,127 @@ import ait.citizens.model.Person;
 import org.junit.jupiter.api.*;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CitizensTest {
-//    LocalDate today = LocalDate.now();
     Citizens citizens;
-    Comparator<Person> comparator = (p1, p2) ->{
-        int result = Integer.compare(p1.getId(),p2.getId());
-        result = result !=0 ? result : p1.getLastName().compareTo(p2.getLastName());
-        return result != 0 ? result : Integer.compare(p1.getAge(), p2.getAge());
-    };
+    static final LocalDate now = LocalDate.now();
+    Person p1;
+
+
+//    @BeforeEach
+//    void setUp() {  // так тоже правильно!!!!
+//        citizens = new CitizensImpl();
+//        citizens.add(new Person(1001, "name1", "lastName1", LocalDate.of(1965, 12, 27)));
+//        citizens.add(new Person(1002, "name2", "lastName2", LocalDate.of(2013, 04, 12)));
+//        citizens.add(new Person(1003, "name3", "lastName3", LocalDate.of(1984, 06, 14)));
+//        citizens.add(new Person(1004, "name4", "lastName1", LocalDate.of(1967, 03, 8)));
+//        citizens.add(new Person(1005, "name2", "lastName4", LocalDate.of(1991, 01, 25)));
+//    }
 
     @BeforeEach
     void setUp() {
-        citizens = new CitizensImpl();
-        citizens.add(new Person(1001, "name1", "lastName1", LocalDate.of(1965, 12, 27)));
-        citizens.add(new Person(1002, "name2", "lastName2", LocalDate.of(2013, 04, 12)));
-        citizens.add(new Person(1003, "name3", "lastName3", LocalDate.of(1984, 06, 14)));
-        citizens.add(new Person(1004, "name4", "lastName1", LocalDate.of(1967, 03, 8)));
-        citizens.add(new Person(1005, "name2", "lastName4", LocalDate.of(1991, 01, 25)));
+        p1 = new Person(1, "Peter", "Jackson", now.minusYears(23));
+        citizens = new CitizensImpl(
+                List.of(p1,
+                        new Person(2, "John", "Smith", now.minusYears(20)),
+                        new Person(3, "Mary", "Jackson", now.minusYears(20)),
+                        new Person(4, "Rabindranate", "Anand", now.minusYears(25))));
     }
 
     @Test
-    void CitizenImpl() {
-        List<Person> person = new ArrayList<>();
-        person.add(new Person(1003, "name3", "lastName3", LocalDate.of(1984, 06, 14)));
-        person.add(new Person(1004, "name4", "lastName1", LocalDate.of(1967, 03, 8)));
-        person.add(new Person(1003, "name2", "lastName4", LocalDate.of(1991, 01, 25)));
-        assertEquals(2, citizens.size());
-
+    void testCitizensImplListOfPerson() {
+        citizens = new CitizensImpl(List.of(p1, p1));
+        assertEquals(1, citizens.size());
     }
 
     @Test
     void add() {
         assertFalse(citizens.add(null));
-        assertFalse(citizens.add(new Person(1002, "name2", "lastName4", LocalDate.of(1991, 01, 25))));
-        assertTrue(citizens.add(new Person(1006, "name6", "lastName2", LocalDate.of(2016, 11, 19))));
-        assertEquals(6, citizens.size());
+        assertFalse(citizens.add(new Person(2, "John", "Smith", now.minusYears(20))));
+        assertEquals(4, citizens.size());
+        assertTrue(citizens.add(new Person(5, "John", "Smith", now.minusYears(20))));
+        assertEquals(5, citizens.size());
     }
 
     @Test
     void remove() {
-        citizens.remove(1005);
+        assertFalse(citizens.remove(5));
         assertEquals(4, citizens.size());
-        assertFalse(citizens.remove(1005));
-        assertNull(citizens.find(1005));
+        assertTrue(citizens.remove(2));
+        assertEquals(3, citizens.size());
     }
 
     @Test
     void findById() {
-        assertNull(citizens.find(2222));
-        assertEquals(citizens.find(1002), new Person(1002, "name2", "lastName2", LocalDate.of(2013, 11, 19)));
+        Person person = citizens.find(1);
+        assertEquals(p1, person);
+        assertEquals(1, person.getId());
+        assertEquals("Peter", person.getFirstName());
+        assertEquals("Jackson", person.getLastName());
+        assertEquals(23, person.getAge());
+        assertNull(citizens.find(5));
     }
 
     @Test
-    void testFindByMinAndMaxAge() {
-        int minAge = LocalDate.MAX.compareTo(LocalDate.now());
-        int maxAge = LocalDate.MIN.compareTo(LocalDate.now());
-        Iterable<Person> expected = citizens.find(minAge, maxAge);
-        Iterable<Person> actual = new ArrayList<>(0);
-        actual.iterator().next().getAge();
-        assertIterableEquals( expected, actual);
+    void testFindByAges() {
+        Iterable<Person> res = citizens.find(20, 23);
+        List<Person> actual = new ArrayList<>();
+        res.forEach(p -> actual.add(p));
+        Collections.sort(actual);
+        Iterable<Person> expected = List.of(
+                new Person(1, "Peter", "Jackson", now.minusYears(23)),
+                new Person(2, "John", "Smith", now.minusYears(20)),
+                new Person(3, "Mary", "Jackson", now.minusYears(20)));
+        assertIterableEquals(expected, actual);
     }
 
     @Test
     void testFindByLastName() {
-        assertNull(citizens.find(null));
-        Iterable<Person> expected = citizens.find("lastName1");
-        Iterable<Person> actual = new ArrayList<>(0);
-        actual.iterator().next().setLastName("lastName1");
-        assertIterableEquals( expected, actual);
+        Iterable<Person> res = citizens.find("Jackson");
+        List<Person> actual = new ArrayList<>();
+        res.forEach(p -> actual.add(p));
+        Collections.sort(actual);
+        Iterable<Person> expected = List.of(
+                new Person(1, "Peter", "Jackson", now.minusYears(23)),
+                new Person(3, "Mary", "Jackson", now.minusYears(20)));
+        assertIterableEquals(expected, actual);
     }
 
     @Test
-    void getAllPersonsSortedById() {
-        assertEquals(citizens.getAllPersonsSortedById(), comparator);
+    void getAllPersonSortedById() {
+        Iterable<Person> actual = citizens.getAllPersonsSortedById();
+        Iterable<Person> expected = List.of(p1,
+                new Person(2, "John", "Smith", now.minusYears(20)),
+                new Person(3, "Mary", "Jackson", now.minusYears(20)),
+                new Person(4, "Rabindranate", "Anand", now.minusYears(25)));
+        assertIterableEquals(expected, actual);
     }
 
     @Test
     void getAllPersonsSortedByAge() {
-        assertEquals(citizens.getAllPersonsSortedByAge(), comparator);
+        Iterable<Person> actual = citizens.getAllPersonsSortedByAge();
+        Iterable<Person> expected = List.of(
+                new Person(2, "John", "Smith", now.minusYears(20)),
+                new Person(3, "Mary", "Jackson", now.minusYears(20)),
+                p1, new Person(4, "Rabindranate", "Anand", now.minusYears(25)));
+        assertIterableEquals(expected, actual);
     }
 
     @Test
-    void getAllPersonsSortedByLastName() {
-        assertEquals(citizens.getAllPersonsSortedByLastName(), comparator);
+    void getAllPersonSortedByLastName() {
+        Iterable<Person> actual = citizens.getAllPersonsSortedByLastName();
+        String name = "";
+        for (Person person : actual) {
+            assertTrue(person.getLastName().compareTo(name) >= 0);
+            name = person.getLastName();
+        }
     }
 
     @Test
     void size() {
-        assertEquals(5, citizens.size());
+        assertEquals(4, citizens.size());
     }
 }
