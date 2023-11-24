fab3dc30e896b3ac81bc44a86ee15e49bb65159a
Igor Volotovskyi <ihor737@gmail.com>
2023-11-24 12:08:38 +0100

    added Git example 05

--- basic_programming_Java/Git/Git_example_05/src/Main.java
+++ basic_programming_Java/Git/Git_example_05/src/Main.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+        /*
+        1. Create remote repository
+        2. Create local and enter next command
+        3. git init
+        4. create gitignore  /touch .gitignore/
+        5.git remote add remote git@github.com:IgorVolot/example_05.git
+        6. git add .
+        7. git commit -m 'init'
+        8. git push origin main или git push и выбираем из предложенных вариантов
+        9. git pull origin main - подтянуть
+         */
+
+
+    }
 }
