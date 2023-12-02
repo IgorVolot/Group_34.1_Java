fd790f921eac5839af8c350ffedf7722c813e470
Igor Volotovskyi <ihor737@gmail.com>
2023-12-02 14:00:41 +0100

    HW 57 corrected.

--- basic_programming_Java/Homework/Homework_57_CompareIO/src/ait/byteio/CompareAppl.java
+++ basic_programming_Java/Homework/Homework_57_CompareIO/src/ait/byteio/CompareAppl.java
@@ -3,6 +3,7 @@ package ait.byteio;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Arrays;
 
         /*
         В классе CompareAppl реализовать логику сравнения двух файлов в методе main. Имена файлов принимать через массив args.
@@ -27,15 +28,26 @@ public class CompareAppl {
              FileInputStream fin3 = new FileInputStream(args[2]); FileInputStream fin4 = new FileInputStream(args[3]);
              FileInputStream fin5 = new FileInputStream(args[4]); FileInputStream fin6 = new FileInputStream(args[5])) {
             int len1 = fin1.available();
+            byte[] arr1 = new byte[len1];
+
             int len2 = fin2.available();
+            byte[] arr2 = new byte[len2];
+
             int len3 = fin3.available();
+            byte[] arr3 = new byte[len3];
+
             int len4 = fin4.available();
+            byte[] arr4 = new byte[len4];
+
             int len5 = fin5.available();
+            byte[] arr5 = new byte[len5];
+
             int len6 = fin6.available();
-            if (len3 == len4 && fin3.read() == fin4.read()) {
+            byte[] arr6 = new byte[len6];
+            if (len3 == len4 && Arrays.equals(arr3, arr4)) {
                 System.out.println("File <" + args[2] + "> and file <" + args[3] + "> are equal.");
             } else {
-                System.out.println("File <" + args[2] + "> and file <" + args[3] + "> are NOT equal.");
+                System.out.println("Files are NOT equal.");
             }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
