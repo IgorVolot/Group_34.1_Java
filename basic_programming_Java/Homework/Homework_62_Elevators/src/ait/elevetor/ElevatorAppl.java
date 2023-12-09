75995a50fe5008c1bf584693607d906b1e492468
Igor Volotovskyi <ihor737@gmail.com>
2023-12-09 14:46:16 +0100

    HW 62 updated

--- basic_programming_Java/Homework/Homework_62_Elevators/src/ait/elevetor/ElevatorAppl.java
+++ basic_programming_Java/Homework/Homework_62_Elevators/src/ait/elevetor/ElevatorAppl.java
@@ -5,20 +5,23 @@ import ait.elevetor.task.Truck;
 
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
+import java.util.Arrays;
 
 public class ElevatorAppl {
     private static final int N_TRUCK = 1000;
     private static final int N_RACES = 10;
     private static final int CAPACITY = 20;
-    public static void main(String[] args) throws InterruptedException {
-        LocalDateTime start = LocalDateTime.now();
 
+    public static void main(String[] args) throws InterruptedException {
         Elevator lenin = new Elevator("V. I. Lenin");
         Elevator stalin = new Elevator("Y. V. Stalin");
 
         Truck[] trucks = new Truck[N_TRUCK];
+//        for (int i = 0; i < trucks.length; i++) {
+//            trucks[i] = new Truck(N_RACES, CAPACITY, lenin, stalin);
+//        }
         for (int i = 0; i < trucks.length; i++) {
-            if (i % 2 == 0){
+            if (i % 2 == 0) {
                 trucks[i] = new Truck(N_RACES, CAPACITY, lenin);
             } else {
                 trucks[i] = new Truck(N_RACES, CAPACITY, stalin);
@@ -31,14 +34,10 @@ public class ElevatorAppl {
             threads[i].start();
         }
 
-        for (Thread thread: threads) {
+        for (Thread thread : threads) {
             thread.join();
         }
         System.out.println("Elevator " + lenin.getName() + " has " + lenin.getCurrentVolume());
         System.out.println("Elevator " + stalin.getName() + " has " + stalin.getCurrentVolume());
-
-        LocalDateTime stop = LocalDateTime.now();
-        double sec = ChronoUnit.MILLIS.between(start,stop) * 1.;
-        System.out.println(sec);
     }
 }
--- basic_programming_Java/Homework/Homework_62_Elevators/src/ait/elevetor/task/Truck.java
+++ basic_programming_Java/Homework/Homework_62_Elevators/src/ait/elevetor/task/Truck.java
@@ -3,11 +3,20 @@ package ait.elevetor.task;
 import ait.elevetor.model.Elevator;
 
 public class Truck implements Runnable {
-    private static Object monitor = new Object();
+    private static final Object monitor1 = new Object();
+    private static final Object monitor2 = new Object();
     private int nRaces;
-
     private int capacity;
     private Elevator elevator;
+    Elevator lenin = new Elevator("V. I. Lenin");
+    Elevator stalin = new Elevator("Y. V. Stalin");
+
+    public Truck(int nRaces, int capacity, Elevator lenin, Elevator stalin) {
+        this.nRaces = nRaces;
+        this.capacity = capacity;
+        this.lenin = lenin;
+        this.stalin = stalin;
+    }
 
     public Truck(int nRaces, int capacity, Elevator elevator) {
         this.nRaces = nRaces;
@@ -18,14 +27,33 @@ public class Truck implements Runnable {
     @Override
     public void run() {
         for (int i = 0; i < nRaces; i++) {
-            if (i % 2 == 0){
-                elevator.add(capacity);
-            } else {
-                synchronized (monitor){
+            if (elevator.getName().equals("V. I. Lenin")) {
+                synchronized (monitor1) {
+                    elevator.add(capacity);
+                }
+            }
+            if (elevator.getName().equals("Y. V. Stalin")) {
+                synchronized (monitor2) {
                     elevator.add(capacity);
                 }
             }
         }
     }
 
+
+//    @Override
+//    public void run() {
+//        switch (elevator.getName()) {
+//            case ("V. I. Lenin"):
+//                synchronized (monitor1) {
+//                    elevator.add(capacity);
+//                }
+//                break;
+//            case ("Y. V. Stalin"):
+//                synchronized (monitor2) {
+//                    elevator.add(capacity);
+//                }
+//                break;
+//        }
+//    }
 }
