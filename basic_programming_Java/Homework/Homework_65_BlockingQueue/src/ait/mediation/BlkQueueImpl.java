4418cf32fe4cae63a04751f06775521f5a3d71c5
Igor Volotovskyi <ihor737@gmail.com>
2023-12-12 23:49:33 +0100

    HW 65 updated

--- basic_programming_Java/Homework/Homework_65_BlockingQueue/src/ait/mediation/BlkQueueImpl.java
+++ basic_programming_Java/Homework/Homework_65_BlockingQueue/src/ait/mediation/BlkQueueImpl.java
@@ -1,20 +1,57 @@
 package ait.mediation;
 
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class BlkQueueImpl<T> implements BlkQueue<T> {
+    private Queue<T> message;
+    private int maxSize;
+    Lock mutex = new ReentrantLock();
+    Condition producerWaitingCondition = mutex.newCondition();
+    Condition consumerWaitingCondition = mutex.newCondition();
+
     public BlkQueueImpl(int maxSize) {
-        // TODO
-        throw new UnsupportedOperationException("Not implemented");
+        this.message = new LinkedList<>();
+        this.maxSize = maxSize;
     }
 
     @Override
     public void push(T message) {
-        // TODO
-        throw new UnsupportedOperationException("Not implemented");
+        mutex.lock();
+        try {
+            while (this.message.size() != maxSize) {
+                try {
+                    producerWaitingCondition.await();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            this.message.add(message);
+            consumerWaitingCondition.signal();
+        } finally {
+            mutex.unlock();
+        }
     }
 
     @Override
     public T pop() {
-        // TODO
-        throw new UnsupportedOperationException("Not implemented");
+        mutex.lock();
+        try {
+            while (this.message.isEmpty()) {
+                try {
+                    consumerWaitingCondition.await();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            T res = message.remove();
+            producerWaitingCondition.signal();
+            return res;
+        } finally {
+            mutex.unlock();
+        }
     }
 }
