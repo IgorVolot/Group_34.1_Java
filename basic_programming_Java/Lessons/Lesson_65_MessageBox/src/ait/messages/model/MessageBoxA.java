8f316a222ae7533d56adf7e161b9108a3eb042de
Igor Volotovskyi <ihor737@gmail.com>
2023-12-12 11:50:05 +0100

    Lesson 65 added

---
+++ basic_programming_Java/Lessons/Lesson_65_MessageBox/src/ait/messages/model/MessageBoxA.java
@@ -0,0 +1,26 @@
+package ait.messages.model;
+
+public class MessageBoxA implements MessageBox {
+    private String message;
+
+    @Override
+    public synchronized void post(String message) {
+        this.message = message;
+//        this.notify();
+        notify();
+    }
+
+    @Override
+    public synchronized String get() {
+        while (this.message == null){
+            try {
+                this.wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        String res = message;
+        message = null;
+        return res;
+    }
+}
