80a520f95b5bc976b2d63e649421589bbb4b141a
Igor Volotovskyi <ihor737@gmail.com>
2023-12-20 14:24:11 +0100

    Lesson 70 updated

---
+++ basic_programming_Java/Lessons/Lesson_70_ChatClient/src/ait/chat/client/ChatClientAppl.java
@@ -0,0 +1,25 @@
+package ait.chat.client;
+
+import ait.chat.client.task.MessageReceiver;
+import ait.chat.client.task.MessageSender;
+
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class ChatClientAppl {
+    public static void main(String[] args) {
+        String serverHost = "5.tcp.eu.ngrok.io";
+        int port = 17250;
+        try {
+            Socket socket = new Socket(serverHost, port);
+            Thread sender = new Thread(new MessageSender(socket));
+            Thread receiver = new Thread(new MessageReceiver(socket));
+            receiver.setDaemon(true);
+            receiver.start();
+            sender.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
